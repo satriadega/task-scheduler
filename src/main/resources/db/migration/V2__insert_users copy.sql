26176edc18ccfd2901cebbf908c2d516227fec59
satriadega <satria.pandega@gmail.com>
2024-02-07 21:20:37 +0700

    initial commit

---
+++ src/main/resources/db/migration/V2__insert_users copy.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email) VALUES
+  ('John Doe', 'john@example.com'),
+  ('Jane Smith', 'jane@example.com'),
+  ('Alice Johnson', 'alice@example.com'),
+  ('Bob Williams', 'bob@example.com'),
+  ('Emma Brown', 'emma@example.com');
